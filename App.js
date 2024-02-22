1e794cced49fc023b7c35625218bce79c346e9d4
mariosknl <mariosknl@outlook.com>
2024-02-22 09:50:26 +0200

    render one exercise

--- App.js
+++ App.js
@@ -3,9 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import exercises from "./assets/data/exercises.json";
 
 export default function App() {
+  const exercise = exercises[0];
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubTitle}>
+          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
+        </Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "gainsboro",
     justifyContent: "center",
+    padding: 10,
+  },
+  exerciseContainer: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  exerciseSubTitle: {
+    color: "dimgray",
   },
 });
