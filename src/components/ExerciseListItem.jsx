571b1e7a5941038037b5557928347d5dd9c97815
mariosknl <mariosknl@outlook.com>
2024-02-22 10:30:39 +0200

    setup expo router

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -16,6 +16,15 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     gap: 5,
+    marginHorizontal: 2,
+
+    // shadow
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
   },
   exerciseName: {
     fontSize: 20,
